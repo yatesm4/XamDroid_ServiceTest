6b6f19b40a47de4ed65abd19f1eb9732ef7d0eea
Matthew Yates <yatesmat@msu.edu>
2018-04-20 12:56:32 -0400

    started new project. set up basic foreground service to communicate with the app

---
+++ ServiceTest/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Content.PM;
+using Android.Hardware;
+using Android.Runtime;
+
+namespace ServiceTest
+{
+    [Activity(Label = "ServiceTest", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        Button btn_Start;
+        Button btn_Stop;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            LoadComponents();
+            LoadDelegates();
+        }
+
+        void LoadComponents()
+        {
+            btn_Start = FindViewById<Button>(Resource.Id.btn_start);
+            btn_Stop = FindViewById<Button>(Resource.Id.btn_stop);
+        }
+
+        void LoadDelegates()
+        {
+            btn_Start.Click += delegate
+            {
+                StartService(new Intent(this, typeof(SimpleService)));
+            };
+
+            btn_Stop.Click += delegate
+            {
+                StopService(new Intent(this, typeof(SimpleService)));
+            };
+        }
+    }
+}
+
---
+++ ServiceTest/SimpleService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+using Android.Support.V4.App;
+using TaskStackBuilder = Android.Support.V4.App.TaskStackBuilder;
+
+namespace ServiceTest
+{
+    [Service]
+    public class SimpleService : Service
+    {
+        static readonly string TAG = "X:" + typeof(SimpleService).Name;
+        static readonly int TimerWait = 4000;
+        Timer timer;
+        DateTime startTime;
+        bool isStarted = false;
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+        }
+
+        [return: GeneratedEnum]
+        public override StartCommandResult OnStartCommand(Intent intent, [GeneratedEnum] StartCommandFlags flags, int startId)
+        {
+            Log.Debug(TAG, $"OnStartCommand called at {startTime}, flags={flags}, startid={startId}");
+            if (isStarted)
+            {
+                TimeSpan runtime = DateTime.UtcNow.Subtract(startTime);
+                Log.Debug(TAG, $"This service was already started, it's been running for {runtime:c}.");
+            }
+            else
+            {
+                startTime = DateTime.UtcNow;
+                Log.Debug(TAG, $"Starting the service, at {startTime}.");
+                timer = new Timer(HandleTimerCallback, startTime, 0, TimerWait);
+                isStarted = true;
+
+                var ongoing = new Notification(Resource.Drawable.MSU_Helmet, "SimpleService in foreground");
+                var pendingIntent = PendingIntent.GetActivity(this, 0, new Intent(this, typeof(MainActivity)), 0);
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .SetAutoCancel(true)                   // Dismiss on click
+                .SetSound(Android.Media.RingtoneManager.GetDefaultUri(Android.Media.RingtoneType.Notification))
+                .SetContentTitle("SimpleService")
+                .SetSmallIcon(Resource.Drawable.MSU_Helmet)
+                .SetContentText("SimpleService is running in the foreground");
+
+                StartForeground((int)NotificationFlags.ForegroundService, builder.Build());
+            }
+            return StartCommandResult.RedeliverIntent;
+        }
+
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+
+        public override void OnDestroy()
+        {
+            timer.Dispose();
+            timer = null;
+            isStarted = false;
+
+            TimeSpan runtime = DateTime.UtcNow.Subtract(startTime);
+            Log.Debug(TAG, $"Simple Service destroyed at {DateTime.UtcNow} after running for {runtime:c}.");
+            base.OnDestroy();
+        }
+
+        void HandleTimerCallback(object state)
+        {
+            TimeSpan runTime = DateTime.UtcNow.Subtract(startTime);
+            Log.Debug(TAG, $"This service has been running for {runTime:c} (since ${state}).");
+        }
+    }
+}
